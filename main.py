91c866a15a24c0ccebca2382b0f13227a60bf106
NinerMG <maciej.gala@hotmail.com>
2025-03-06 13:20:57 +0100

    Add description

---
+++ main.py
@@ -0,0 +1,42 @@
+import random
+
+
+def main():
+   evenNumber()
+
+def greetings():
+    name = input("Enter your name")
+    print(f"Hello,", name)
+
+def checkType():
+    name = "Maciek"
+    number = 2
+    decimalNumber = 2.0
+
+    print(type(name))
+    print(type(number))
+    print(type(decimalNumber))
+
+def isFullAge():
+    age = int(input("Enter your age"))
+    if age >= 18:
+        print("You welcome")
+    else:
+        print("You are under age!")
+
+def randomList():
+    owoce = ["jabłko", "banan", "gruszka", "truskawka", "borówka", "wiśnia"]
+    print(random.choice(owoce))
+
+def evenNumber():
+    for i in range(1, 20):
+        if i % 2 == 0:
+            print(i)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
