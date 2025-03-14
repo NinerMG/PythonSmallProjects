7029f1f672d7ae9656524c0afb66ff9c86771ae1
NinerMG <maciej.gala@hotmail.com>
2025-03-14 16:35:19 +0100

    Day 17 Exercises

--- Day17/quiz-game-start/question_model.py
+++ Day17/quiz-game-start/question_model.py
@@ -1,6 +1,5 @@
 class Question:
-    def __init__(self):
-        self.text
-        self.answer
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
 
-new_q = Question("dsadsa", "False")
