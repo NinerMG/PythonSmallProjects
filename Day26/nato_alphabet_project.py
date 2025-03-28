66489e9350e1eb7024c7576ec0982586084a4015
NinerMG <maciej.gala@hotmail.com>
2025-03-28 08:46:27 +0100

    Day 26 correcting errors

--- Day26/nato_alphabet_project.py
+++ Day26/nato_alphabet_project.py
@@ -1,6 +1,6 @@
 import pandas
 
-data = pandas.read.csv("nato_phonetic_alphabet.csv")
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
 print(data.to_dict())
 
 # Keyword Method with iterrows()
@@ -9,8 +9,8 @@ print(data.to_dict())
 #TODO 1. Create a dictionary in this format:
 #{"A": "Alfa", "B": "Bravo"}
 
- phonetic_dict = {row.letter : row.code for (index, row) in data.iterrows()}
- print(phonetic_dict)
+phonetic_dict = {row.letter : row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 word = input("Enter a word").upper()
