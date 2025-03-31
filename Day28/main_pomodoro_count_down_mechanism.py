5f832812ef2ef5418ecbb628263e47e866372ba6
NinerMG <maciej.gala@hotmail.com>
2025-03-31 17:12:23 +0200

    Day 28 pomodoro app

--- Day28/main_pomodoro_count_down_mechanism.py
+++ Day28/main_pomodoro_count_down_mechanism.py
@@ -9,8 +9,18 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps =0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- #
+
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_mars.config(text="")
+    global  reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
@@ -22,9 +32,16 @@ def start_timer():
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
 
-    count_down(work_sec)
-    count_down(long_break_sec)
-    count_down(short_break_sec)
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg = RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
@@ -37,7 +54,15 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count -1)
+        global timer
+        timer = window.after(1000, count_down, count -1)
+    else:
+        start_timer()
+        mark = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            mark += "✔"
+        check_mars.config(text=mark)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -65,10 +90,10 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset",highlightthickness=0)
+reset_button = Button(text="Reset",highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_mars = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_mars = Label(fg=GREEN, bg=YELLOW)
 check_mars.grid(column=1, row=3)
 
 window.mainloop()
