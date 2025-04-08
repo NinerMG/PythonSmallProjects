d909b1e5666e423b734ad85d9ea8b0ee32fb2c3d
Maciej Gala <maciej.gala@comarch.com>
2025-04-08 11:41:03 +0200

    Day 31, project - flash card

--- Day31/main.py
+++ Day31/main.py
@@ -6,26 +6,45 @@ import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-data = pandas.read_csv("french_words.csv")
-to_learn = data.to_dict(orient="records")
-current_card = {}
+try:
+    #data = pandas.read_csv("data/french_words.csv")
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data =  pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+    current_card = {}
 
 
 def next_card():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="English")
-    canvas.itemconfig(card_word, text=current_card["English"])
+    canvas.itemconfig(card_title, text="English", fill="black")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="black")
+    canvas.itemconfig(card_background, image=card_front)
+    flip_timer = window.after(3000, func=flip_card)
 
 def flip_card():
-    canvas.itemconfig(card_title, text="Polish")
-    canvas.itemconfig(card_word, text=current_card["Polish"])
+    canvas.itemconfig(card_title, text="Polish", fill="white")
+    canvas.itemconfig(card_word, text=current_card["Polish"], fill="white")
+    canvas.itemconfig(card_background, image=card_back)
+
+def is_known():
+    to_learn.remove(current_card)
+    # print(len(to_learn))
+
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+
+    next_card()
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50,background=BACKGROUND_COLOR)
 
-window.after(3000,func=flip_card)
+flip_timer = window.after(3000,func=flip_card)
 
 canvas = Canvas(width=800,height=526)
 
@@ -36,14 +55,14 @@ wrong_button_image = PhotoImage(file="images/wrong.png")
 
 
 # front card and text
-canvas.create_image(400,263, image=card_front)
+card_background = canvas.create_image(400,263, image=card_front)
 card_title = canvas.create_text(400,150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400,263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0,row=0,columnspan=2)
 
 # adding button
-right_button = Button(image=right_button_image, highlightthickness=0, command=next_card)
+right_button = Button(image=right_button_image, highlightthickness=0, command=is_known)
 wrong_button = Button(image=wrong_button_image, highlightthickness=0, command=next_card)
 
 right_button.grid(row=1, column=1)
