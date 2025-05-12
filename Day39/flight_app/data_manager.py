ee18926d84f131de6c7eb0a59d6f4ec1eeb9bca3
NinerMG <maciej.gala@hotmail.com>
2025-05-12 22:35:36 +0200

    Day 39

--- Day39/flight_app/data_manager.py
+++ Day39/flight_app/data_manager.py
@@ -1,3 +1,15 @@
+import requests
+
+SHEETY_ENDPOINT = "https://api.sheety.co/6bbf5e5da8baa33e97bf83894678fb1d/mgFlight/prices"
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        pass
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_ENDPOINT)
+        response.raise_for_status()
+        data = response.json()
+        return data["prices"]
+
--- Day39/flight_app/main.py
+++ Day39/flight_app/main.py
@@ -1,1 +1,8 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+#This file will need to use the DataManager,FlightSearch, FlightData,
+# NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+from pprint import pprint
+
+data_manger = DataManager()
+sheet_data = data_manger.get_destination_data()
+pprint(sheet_data)
