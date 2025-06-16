257bf1be0f7a0fce80f25c18696750ad14d3a4a0
Maciej Gala <maciej.gala@comarch.com>
2025-06-16 09:49:12 +0200

    Day 57 templating with jinja exercises

---
+++ Day57/init_tutorial.py
@@ -0,0 +1,8 @@
+# url building and templating with jinja flask app
+# jinja -> język szablonowy
+# projekt prostego bloga
+
+# https://agify.io/
+# https://genderize.io/
+
+# https://flask.palletsprojects.com/en/stable/quickstart/#routing
