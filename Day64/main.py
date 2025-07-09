e6ea2d400229c049f455208b7fd293332cdb19ea
Maciej Gala <maciej.gala@comarch.com>
2025-07-09 14:04:06 +0200

    Day 64 editing rating and review

--- Day64/main.py
+++ Day64/main.py
@@ -47,6 +47,13 @@ class Movie(db.Model):
 with app.app_context():
     db.create_all()
 
+
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10 e.g. 7.5")
+    review = StringField("Your Review")
+    submit = SubmitField("Done")
+
+
 ## After adding the new_movie the code needs to be commented out/deleted.
 ## So you are not trying to add the same movie twice. The db will reject non-unique movie titles.
 # new_movie = Movie(
@@ -79,6 +86,17 @@ def home():
     all_movies = result.scalars().all()
     return render_template("index.html", movies=all_movies)
 
+@app.route("/edit", methods=["GET", "POST"])
+def rate_movie():
+    form = RateMovieForm()
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    if form.validate_on_submit():
+        movie.rating = float(form.rating.data)
+        movie.review = form.review.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html", movie=movie, form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
